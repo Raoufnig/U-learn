11cf2d2509016c7e2cd6e4d57f1e1d066ac0e912
Tagne237 <elohim.tagne@institutsaintjean.org>
2022-11-01 18:00:41 -0100

    update video

--- src/assets/js/videobg.js
+++ src/assets/js/videobg.js
@@ -54,17 +54,17 @@ this.player.seekTo(0)}}}})(void 0);
 
 
         jQuery(document).ready(function($) {
-            var Video_back = new video_background($("#home"), { 
+            var Video_back = new video_background($("#home"), {
                 "position": "absolute", //Follow page scroll
                 "z-index": "-1",        //Behind everything
                 "loop": true,           //Loop when it reaches the end
                 "autoplay": true,       //Autoplay at start
                 "muted": true,          //Muted at start
-                "mp4":"upload/preview.mp4" ,     //Path to video mp4 format
-                "ogg":"upload/preview.ogg" ,     //Path to video ogg format
-                "webm":"upload/preview.webm" ,     //Path to video webm format
+                "mp4":"/assets/upload/preview.mp4" ,     //Path to video mp4 format
+                "ogg":"/assets/upload/preview.ogg" ,     //Path to video ogg format
+                "webm":"/assets/upload/preview.webm" ,     //Path to video webm format
                 "video_ratio": 1.7778,              // width/height -> If none provided sizing of the video is set to adjust
-                "fallback_image": "images/dummy.png",   //Fallback image path
+                "fallback_image": "/assets/images/dummy.png",   //Fallback image path
                 "priority": "html5"             //Priority for html5 (if set to flash and tested locally will give a flash security error)
             });
         });
