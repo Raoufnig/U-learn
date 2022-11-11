02c65a83df749c1c4483dacb6c132a65f6985a75
Tagne237 <elohim.tagne@institutsaintjean.org>
2022-11-11 17:38:37 -0100

    page accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccueilComponent
+    AccueilComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
